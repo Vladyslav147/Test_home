59dda365324d4d5a38b54ad9d21d53dae60c706f
Vlad <vladborohta2020@gamail.com>
2025-11-17 20:13:43 +0200

    d

--- Home1/models.py
+++ Home1/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class UserProfile(models.Model): # Лучше назвать модель в соответствии с её целью
     name = models.CharField(max_length=100) # Вместо text
     phone = models.IntegerField() # Вместо number
-
+    email = models.EmailField()
     
--- Home1/views.py
+++ Home1/views.py
@@ -8,7 +8,28 @@ def index(request):
     return render(request, template_name="index.html")
 
 def register(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('name')
+        user_phone = request.POST.get('phone')
+        user_email = request.POST.get('email')
+
+        UserProfile.objects.create(name = user_name, phone = user_phone, email = user_email)
     return render(request, template_name="register.html")
 
 def users(request):
     return render(request, template_name="users.html")
+
+"""
+def register(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('name')
+        user_phone = request.POST.get('phone') #.get - пишется для того что бы есле не найдет такого поля просто вывело none, Можно и без .get просто при случае когда данных не найдет выведет ошыбку (но только не в () а в [])
+
+        UserProfile.objects.create( # С помощью objects можно: создавать новые записи (create), получать все записи (all), фильтровать записи (filter), …и многое другое.
+            name = user_name,
+            phone = user_phone
+        )
+        return redirect('index')
+
+    return render(request, template_name="register.html")
+"""
